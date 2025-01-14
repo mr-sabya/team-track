5a495250f31d3c7469fb9a8b7eacb22c6c819dda
Sabya Roy <68545445+mr-sabya@users.noreply.github.com>
2025-01-14 22:47:07 +0600

    restore backlup

--- resources/views/livewire/backup/database-backup.blade.php
+++ resources/views/livewire/backup/database-backup.blade.php
@@ -1,13 +1,49 @@
 <div>
-    <div class="d-flex justify-content-between mb-3">
-        <h3>Database Backups</h3>
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="card">
+                <div class="card-body">
+                    <!-- Button to create a backup -->
+                    <button class="btn btn-primary" wire:click="createBackup" wire:loading.attr="disabled">
+                        <span wire:loading wire:target="createBackup">Creating Backup...</span>
+                        <span wire:loading.remove wire:target="createBackup">Create Backup</span>
+                    </button>
+
+                    <!-- List of backups -->
+                    <h4 class="mt-4">Available Backups</h4>
+                    <ul class="list-group">
+                        @foreach($backups as $backup)
+                        <li class="list-group-item d-flex justify-content-between align-items-center">
+                            {{ basename($backup) }}
         <div>
-            <button class="btn btn-primary" wire:click="createBackup" wire:loading.attr="disabled">
-                @if($isBackingUp)
-                Creating Backup...
-                @else
-                Create Backup
-                @endif
+                                <button class="btn btn-success btn-sm" wire:click="downloadBackup('{{ $backup }}')">
+                                    Download
+                                </button>
+                                <button class="btn btn-danger btn-sm" wire:click="deleteBackup('{{ $backup }}')">
+                                    Delete
+                                </button>
+                            </div>
+                        </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-6">
+            <div class="card">
+                <div class="card-body">
+
+                    <!-- Restore Backup -->
+                    <h4 class="mt-4">Restore Backup</h4>
+                    <form wire:submit.prevent="restoreBackup">
+                        <div class="form-group">
+                            <input type="file" class="form-control" wire:model="restoreFile" accept=".sql">
+                            @error('restoreFile') <span class="text-danger">{{ $message }}</span> @enderror
+                        </div>
+                        <button type="submit" class="btn btn-warning mt-2" wire:loading.attr="disabled">
+                            <span wire:loading wire:target="restoreBackup">Restoring...</span>
+                            <span wire:loading.remove wire:target="restoreBackup">Restore Backup</span>
             </button>
             <div wire:loading wire:target="createBackup" style="height: 34px;">
                 <img src="{{ url('assets/images/loading.gif') }}" alt="" style="height: 100%;">
